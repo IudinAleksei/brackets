766bfdff9dadefece3051091a5018b3eebc10c7d
Alexey_Yudin <aviordream@gmail.com>
2020-03-10 21:01:23 +0300

    feat: add test function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let out = str;
+    for (let i = 0; i <= str.length / 2; i++) {
+        bracketsConfig.forEach((item) => {
+            out = out.replace(item[0] + item[1], '');
+
+        });
+    }
+    return !out.length;
 }
